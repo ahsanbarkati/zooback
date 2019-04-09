8690cede38644a14b8f29906c7885815566db345
Naman Jain <jnaman806@gmail.com>
2019-04-09 08:05:51 +0530

    [Add] Get request to bimock

--- routes/routes.js
+++ routes/routes.js
@@ -131,6 +131,7 @@ router.post('/forgetpass', function (req, res, next) {
 	
 });
 
+	var request=require('request'); 
 router.get('/request', function (req, res, next) {
 	console.log("requesting ride");
 	Bike.findOne({status:0},function(err,data){
@@ -143,6 +144,13 @@ router.get('/request', function (req, res, next) {
 			console.log(data);
 			res.send({"bikeid":data.bike_id, "bikename":data.bikename, "lat":data.location.Lat, "long":data.location.Lon});
 		}
+	});
+	Bike.findOne({status:0},function(err,data){
+		console.log("Printing to:")
+		console.log('http://0.0.0.0:900'+String(data.bike_id)+'/requestRide/'+String(data.bike_id))
+		request.get('http://0.0.0.0:900'+String(data.bike_id)+'/requestRide/'+String(data.bike_id), function (error, response) { 
+			console.error('error:', error); 
+		})
 	});
 });
 
